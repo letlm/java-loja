2370b83c51220e71f52fbcb94c2ab65ddd85d521
letlm <lealmleticia@gmail.com>
2023-05-12 19:32:54 -0300

    build: inicio do projeto e aplicação do padrão strategy

---
+++ src/TestesImpostos.java
@@ -0,0 +1,16 @@
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesImpostos {
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
+    }
+}
