846de6b14663d55db7eb915764c8d1f1b146d588
letlm <lealmleticia@gmail.com>
2023-05-14 17:51:04 -0300

    chore: observer aplicado e finalizacao do projeto

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -1,2 +1,33 @@
-package br.com.alura.loja;public class TestesPedidos {
+package br.com.alura.loja;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.GeraPedido;
+import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.Pedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoBancoDeDados;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestesPedidos {
+    public static void main(String[] args) {
+
+        /*String cliente = args[0];
+        BigDecimal valorOrcamento = new BigDecimal(args[1]);
+        int quantidadeItens = Integer.parseInt(args[2])*/
+
+        String cliente = "Ana";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = Integer.parseInt("5");
+
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(new SalvarPedidoBancoDeDados(),
+                new EnviarEmailPedido())
+        );
+        handler.execute(gerador);
+    }
 }
--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,2 +1,27 @@
-package br.com.alura.loja.pedido;public class GeraPedidoHandler {
+package br.com.alura.loja.pedido;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoBancoDeDados;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GeraPedidoHandler {
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
+
+    public void execute(GeraPedido dados){
+        Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
+        LocalDateTime data = LocalDateTime.now();
+
+        Pedido pedido = new Pedido(dados.getCliente(), data, orcamento);
+
+        acoes.forEach(a -> a.executarAcao(pedido));
+
+    }
 }
